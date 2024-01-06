dee8229c07d6aeeab42e25b148b8f6570cfbdf07
Goodnessmbakara <amicablembakara50@gmail.com>
2024-01-06 21:19:42 +0100

    fully functional source code

--- qrcode_app/views.py
+++ qrcode_app/views.py
@@ -11,38 +11,41 @@ from datetime import datetime
 import uuid
 
 class QRCodeDataView(View):
-    def get(self, request):
-        form = QRCodeDataForm()
-        return render(request, 'qrcode_data_form.html', {'form': form})
+   def get(self, request):
+       form = QRCodeDataForm()
+       return render(request, 'qrcode_data_form.html', {'form': form})
 
-    def post(self, request):
-        form = QRCodeDataForm(request.POST)
-        if form.is_valid():
-            instance = form.save()
-            unique_name = f"{datetime.now().strftime('%Y%m%d%H%M%S')}-{uuid.uuid4().hex}.png"
-            qr = qrcode.QRCode(
-                version=1,
-                error_correction=qrcode.constants.ERROR_CORRECT_L,
-                box_size=10,
-                border=4,
-            )
-            qr.add_data(instance.data)
-            qr.make(fit=True)
-            img = qr.make_image(fill_color="black", back_color="white")
+   def post(self, request):
+       form = QRCodeDataForm(request.POST)
+       if form.is_valid():
+           instance = form.save()
+           unique_name = f"{datetime.now().strftime('%Y%m%d%H%M%S')}-{uuid.uuid4().hex}.png"
+           qr = qrcode.QRCode(
+               version=1,
+               error_correction=qrcode.constants.ERROR_CORRECT_L,
+               box_size=10,
+               border=4,
+           )
+           qr.add_data(instance.data)
+           qr.make(fit=True)
+           img = qr.make_image(fill_color="black", back_color="white")
 
-            save_directory = os.path.join(settings.MEDIA_ROOT, 'qrcodes')
-            img_path = os.path.join(save_directory, unique_name)
-            img.save(img_path)
+           save_directory = os.path.join(settings.MEDIA_ROOT, 'qrcodes')
+           os.makedirs(save_directory, exist_ok=True)
+           os.chmod(save_directory, 0o755)
 
-            with open(img_path, 'rb') as img_file:
-                instance.qr_code.save(unique_name, File(img_file), save=True)
+           img_path = os.path.join(save_directory, unique_name)
+           img.save(img_path)
 
-            img_url = request.build_absolute_uri(instance.qr_code.url.lstrip('/'))
+           with open(img_path, 'rb') as img_file:
+               instance.qr_code.save(unique_name, File(img_file), save=True)
 
-            if not (instance.data.startswith('http://') or instance.data.startswith('https://')):
-                # Prepend http:// to instance.data
-                instance.data = 'https://' + instance.data
+           img_url = request.build_absolute_uri(instance.qr_code.url.lstrip('/'))
 
-            return render(request, 'qrcode_image.html', {'img_url': img_url, 'data_url': instance.data, 'qr_code_name': unique_name })
+           if not (instance.data.startswith('http://') or instance.data.startswith('https://')):
+               # Prepend http:// to instance.data
+               instance.data = 'https://' + instance.data
 
-        return render(request, 'qrcode_data_form.html', {'form': form})
+           return render(request, 'qrcode_image.html', {'img_url': img_url, 'data_url': instance.data, 'qr_code_name': unique_name })
+
+       return render(request, 'qrcode_data_form.html', {'form': form})
