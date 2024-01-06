484176957dab7ab03aa486070eb2488ac8db62d4
Goodnessmbakara <amicablembakara50@gmail.com>
2024-01-06 21:03:49 +0100

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install -r requirement.txt
 
 python manage.py collectstatic --no-input
-python manage.py flush
+python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
