07d4f34c7acb345dc087ba9ab44ccde074b25f75
Goodnessmbakara <amicablembakara50@gmail.com>
2024-09-09 14:14:04 +0100

    added a change in the build script

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ python manage.py collectstatic --no-input --clear
 python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
+python manage.py makemigrations qrcode_app
+python manage.py migrate qrcode_app
+ 
